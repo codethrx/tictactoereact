93f6155bca9af2952791bdde850305d57ac77c21
codethrx <tatheermehdi2000@gmail.com>
2023-04-22 17:59:47 +0500

    title updated

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default function App() {
   const playerProps = { winner, icon };
   return (
     <div>
-      <h1>Tic toe</h1>
+      <h1>Tic toe tac v1</h1>
       <Grid {...gridProps} />
       <Player {...playerProps} />
     </div>
